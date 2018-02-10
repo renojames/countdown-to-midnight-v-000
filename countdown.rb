43b1777ac1743d31aeb1ace2e80612c13d4708ce
Reno Sorensen <renojames19@gmail.com>
2018-02-10 00:29:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ n = 10
 
 def countdown(n)
   while n > 0
-    puts "#{n} SECOND(S!)"
+    puts "#{n} SECOND(S)!"
     n -= 1
   end
   "HAPPY NEW YEAR!"
@@ -12,7 +12,7 @@ end
 
 def countdown_with_sleep(n)
   while n > 0
-    puts "#{n} SECOND(S!)"
+    puts "#{n} SECOND(S)!"
     n -= 1
     sleep 1
   end
